278fa9d11774a1ed8941777f433620483d1cd530
sgonzalez138 <60118025+sgonzalez138@users.noreply.github.com>
2020-04-21 04:53:29 -0400

    Update DashboardLayout.vue

--- src/pages/Layout/DashboardLayout.vue
+++ src/pages/Layout/DashboardLayout.vue
@@ -13,7 +13,7 @@
       </sidebar-link>
       <sidebar-link to="/user">
         <md-icon>person</md-icon>
-        <p>Teams</p>
+        <p>Team</p>
       </sidebar-link>
       <sidebar-link to="/table">
         <md-icon>content_paste</md-icon>
