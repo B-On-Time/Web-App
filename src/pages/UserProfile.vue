593c7bbb6a34fd7c1a5d20eff5678d98d890288c
sgonzalez138 <60118025+sgonzalez138@users.noreply.github.com>
2020-04-21 04:22:24 -0400

    Update UserProfile.vue

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -1,124 +1,206 @@
 <template>
-  <div class="content">
-    <div class="md-layout">
-      <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"></div>
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
+<div class="content">
+  <div class="md-layout">
+    <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"></div>
+    <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50">
 
+      <!-- BUTTON BELOW IS STRICTLY USED FOR TESTING MULTIPLE TABLES -->
+      <!-- <md-button v-on:click="addTeam" class="md-fab md-primary"> -->
+
+      <b-container class="bv-example-row" fluid="md">
+        <div>
+          <b-row>
+            <b-col>
+              <h1 class="header">Add New Employee</h1>
+            </b-col>
+            <b-col>
+              <md-button v-on:click="addEmployee" class="md-fab md-primary">
+                <md-icon class="add">add</md-icon>
+              </md-button>
+            </b-col>
+          </b-row>
+        </div>
+      </b-container>
       <div v-for="(team, teamindex) in teams" :key="teamindex">
         <md-card class="card">
-        <md-card-header data-background-color="purple">
-          <h1 class="md-title"> Team {{ teamindex + 1 }}
-            <!-- PROPER BUTTON FOR ADDING NEW EMPLOYEE: -->
-            <!-- <md-button v-on:click="addEmployee" class="md-fab md-primary"> -->
-            <!-- BELOW SHOULD BE ELSEWHERE. USED FOR TESTING MULTIPLE TABLES -->
-              <md-button v-on:click="addTeam" class="md-fab md-primary">
-            <md-icon class="add">add</md-icon>
-            </md-button>
-          </h1>
-        </md-card-header>
+          <md-card-header data-background-color="purple">
+            <!-- header for multiple teams implementation -->
+            <!-- <h1 class="md-title"> Team {{ teamindex + 1 }} -->
+            <!-- Header for single team implementation -->
+            <h1 class="md-title"> Patrick {{ team.name }}</h1>
+          </md-card-header>
 
-        <md-card-content>
-          <!-- Takes you to that particular employees profile page when clicked on -->
-          <md-table
-            class="table"
-            table-header-color="purple"
-            md-card @md-selected="goToProfile"
-          >
-            <md-table-row slot="md-table-row" v-for="(member, memberindex) in team" :key="teamindex-memberindex" md-selectable="single">
-              <md-table-cell
-                md-label="ID"
-                md-sort-by="id"
-                v-model = "member.id"
-                md-numeric>{{ member.id }}
-              </md-table-cell>
+          <md-card-content>
+            <!-- Takes you to that particular employees profile page when clicked on -->
+            <md-table class="table" table-header-color="purple" md-card @md-selected="goToProfile">
+              <md-table-row slot="md-table-row" md-selectable="single">
+                <md-table-cell md-label="Name">
+                  Date
+                </md-table-cell>
+                <md-table-cell md-label="Status">
+                  Clock In
+                </md-table-cell>
+                <md-table-cell md-label="Status">
+                  Clock Out
+                </md-table-cell>
+                <md-table-cell md-label="Status">
+                  Work Time
+                </md-table-cell>
+                <md-table-cell md-label="Status">
+                  Break Time
+                </md-table-cell>
+              </md-table-row>
+              <md-table-row slot="md-table-row" v-for="(employee, employeeindex) in team.employees" :key="employeeindex+10" md-selectable="single">
+                <md-table-cell md-label="ID" md-sort-by="id" v-model="employee.id">{{ employee.id }}
+                </md-table-cell>
 
-              <md-table-cell
-                md-label="Name"
-                v-model = "member.name"
-                md-sort-by="name">{{ member.name }}
-              </md-table-cell>
+                <md-table-cell md-label="Name" v-model="employee.name" md-sort-by="name">{{ employee.name }}
+                </md-table-cell>
 
-              <md-table-cell md-label="Status">
-                <p class="status" v-if="member.status == 'true'">Clocked In</p>
-                <p v-else>Clocked Out</p>
-              </md-table-cell>
+                <md-table-cell md-label="Status">
+                  <p class="status" v-if="employee.status == 'true'">Clocked In</p>
+                  <p v-else>Clocked Out</p>
+                </md-table-cell>
 
-              <md-table-cell md-label="Job Title" v-model = "member.title" md-sort-by="title">{{ member.title }}</md-table-cell>
-            </md-table-row>
-          </md-table>
-        </md-card-content>
-      </md-card>
-    </div>
-    </div>
+                <md-table-cell md-label="Job Title" v-model="employee.title" md-sort-by="title">{{ employee.title }}</md-table-cell>
+              </md-table-row>
+            </md-table>
+          </md-card-content>
+        </md-card>
+      </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'TableSingle',
-    data: () => ({
-      selected: {},
+export default {
+  name: 'TableSingle',
+  data: () => ({
+    selected: {},
 
-      teams:[
+    teams: {
+      period_start: '08/14/2020',
+      period_end: '08/28/2020',
+      reports: [ // 0 or more
         {
-          team:[{
-            id: "",
-            name: "",
-            status: "",
-            title: "",
-          }]
+          meta: {
+            userID: '234',
+            fullName: 'Joe Phil',
+            totals: {
+              work: 32,
+              pto: 65,
+              upto: 534,
+              admin: 44
+            }
+          },
+          detail: [ // 0 or more
+            {
+              date: '08/27/2020',
+              type: 'WORK',
+              clockIn: '3:00 PM',
+              clockOut: '4:00 PM',
+              billableMins: 14,
+              breakTotalMins: 57,
+              breaks: [ // 0 or more
+                {
+                  breakStart: '3:30 PM',
+                  breakEnd: '3:40 PM',
+                  breakMins: 10
+                }
+              ]
+
+            }
+          ]
+        },
+        {
+          meta: {
+            userID: '234',
+            fullName: 'Joe Phil1',
+            totals: {
+              work: 321,
+              pto: 651,
+              upto: 5341,
+              admin: 441
+            }
+          },
+          detail: [ // 0 or more
+            {
+              date: '08/27/2020',
+              type: 'WORK',
+              clockIn: '3:00 PM',
+              clockOut: '4:00 PM',
+              billableMins: 141,
+              breakTotalMins: 571,
+              breaks: [ // 0 or more
+                {
+                  breakStart: '3:30 PM',
+                  breakEnd: '3:40 PM',
+                  breakMins: 10
+                }
+              ]
+
+            }
+          ]
         }
-      ],
-    }),
+      ]
+    }
+  }),
 
-    methods: {
-      addEmployee: function (event)  {
-        this.$router.push("/newUser");
-      },
+  methods: {
+    addEmployee: function (event) {
+      this.$router.push("/newUser");
+    },
 
-      addTeam: function (event) {
-        this.teams.push({
-          team:[{
-            id: "",
-            name: "",
-            status: "",
-            title: "",
-          }]
+    addTeam: function (event) {
+      this.teams.push({
+        team: [{
+          id: "",
+          name: "",
+          status: "",
+          title: "",
+        }]
 
-        })
-      },
+      })
+    },
 
-      goToProfile (member) {
-        this.$router.push("/profile");
-        // Ties together with Victors part
-      }
+    goToProfile(employee) {
+      this.$router.push("/profile");
+      // Ties together with Victors part
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .table{
-    width: 750px;
-    margin-left: -21px;
-  }
+.header{
+  font-size: 50px;
+  color: white;
+  letter-spacing: -2px;
+}
 
-  .card{
-    width: 750px;
-    margin-left: -120px;
-  }
+.table {
+  width: 750px;
+  margin-left: -21px;
+}
 
-  .add{
-    margin-right: 19px;
-  }
+.card {
+  width: 750px;
+  margin-left: -120px;
+}
 
-  .status{
-    margin-top: 1px;
-    color: purple;
-    font-weight: bold;
-  }
+.add {
+  margin-right: 19px;
+}
 
-  .md-fab{
-    margin-left: 500px;
-  }
+.status {
+  margin-top: 1px;
+  color: purple;
+  font-weight: bold;
+}
+
+.md-fab {
+  margin-left: 500px;
+}
 </style>
+
